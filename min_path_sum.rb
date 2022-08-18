367eeb65e4a32aa2576a77505615371fa591d776
Guilherme Maranhão <guilhermemaranhao@mpgo.mp.br>
2022-08-18 10:25:23 -0300

    coding tests

--- min_path_sum.rb
+++ min_path_sum.rb
@@ -3,38 +3,27 @@ class MinPathSum
   # @return an integer
   def minPathSum(a)
 
-    right_results = []
-    down_results = []
-    right_results << iterate_matrix(a[0][0], 0, 1, a)
-    down_results << iterate_matrix(a[0][0], 1, 0, a)
+    @@results = []
+    iterate_matrix(a[0][0], 0, 1, a)
+    iterate_matrix(a[0][0], 1, 0, a)
 
-    min_right = right_results[0]
-    for i in 1..right_results.size-1
-      if (right_results[i] < min_right)
-        min_right = right_results[i]
+    min_result = @@results[0]
+    for i in 1..@@results.size-1
+      if (@@results[i] < min_result)
+        min_result = @@results[i]
       end
     end
 
-    min_down = down_results[0]
-    for i in 1..down_results.size-1
-      if (down_results[i] < min_down)
-        min_down = down_results[i]
-      end
-    end
-
-    if (min_right < min_down)
-      return min_right
-    else
-      return min_down
-    end
+    return min_result
 
   end
 
   def iterate_matrix(previous_sum, row_index, column_index, a)
 
+
     # End of Matrix
     if (a[row_index].size-1 == column_index && a.size-1 == row_index)
-      return previous_sum + a[row_index][column_index]
+      @@results << previous_sum + a[row_index][column_index]
     end
 
     current_sum = previous_sum + a[row_index][column_index]
@@ -49,10 +38,9 @@ class MinPathSum
       iterate_matrix(current_sum, row_index+1, column_index, a)
     end
 
+
   end
 
     s = MinPathSum.new
-    matrix = ARGV[0]
-    p matrix
-    p s.minPathSum(matrix)
+    p s.minPathSum([[3,8,0],[2,9,1],[1,7,4]])
 end
